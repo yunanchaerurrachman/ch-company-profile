845fc7d1edf967b8f4349bd7da63f81b78e1c04d
yunanchaerurrachman <yunanchaerurrachman7@gmail.com>
2023-04-01 20:30:55 +0700

    membuat folder dan memperbaiki file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.{html,js}'],
+  content: ['./dist/**/*.{html,js}'],
   theme: {
     extend: {
       fontFamily: {
